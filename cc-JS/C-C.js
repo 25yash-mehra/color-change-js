024658aeb6df00778348ab407b9c35dea3f450fb
red wolf <myash7535@gmail.com>
2023-11-17 11:20:29 +0530

    First-Project-Complete

--- cc-JS/C-C.js
+++ cc-JS/C-C.js
@@ -1,8 +1,8 @@
 var index = 0;
-function colourchange(){
-    var colors = ["red","green","blue","pink","purple","brown"];
+function colourChange(){
+    var colors = ["red","green","blue","pink","purple","brown","violet","orange","yellow","lightblue","aqua","darkgreen","lightgreen","black"];
     document.getElementsByTagName("body")[0].style.background = colors[index++];
-    if(index > colors.length -1);
-        // index = 0;
+        if(index > colors.length - 1) 
+        index = 0;
 }
 
